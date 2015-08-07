82debee28a6ea5572fce0570d21d16d177dabbfc
Fabian Vogelsteller <fabian@frozeman.de>
2015-08-07 22:20:25 +0200

    add helper files

---
+++ ticker.js
@@ -0,0 +1,29 @@
+// Price ticker
+EthTools.ticker = new Mongo.Collection('ethereum_price_ticker', {connection: null});
+if(Meteor.isClient)
+    new PersistentMinimongo(EthTools.ticker);
+
+var updatePrice = function(e, res){
+    if(!e && res.statusCode === 200 && res.data && _.isEmpty(res.data.error)){
+        _.each(res.data.result, function(item, key){
+            var name = key.replace('XETHZ','').toLowerCase();
+
+            if(key === 'XETHXXBT')
+                name = 'btc';
+
+            EthTools.ticker.upsert(name, {$set: {
+                price: item.c[0]
+            }});
+
+        });
+    }
+};
+
+// update right away
+HTTP.get('https://api.kraken.com/0/public/Ticker?pair=XETHZEUR,XETHZUSD,XETHZGBP,XETHZJPY,XETHZCAD,XETHXXBT', updatePrice);
+    
+
+// update prices
+Meteor.setInterval(function(){
+    HTTP.get('https://api.kraken.com/0/public/Ticker?pair=XETHZEUR,XETHZUSD,XETHZGBP,XETHZJPY,XETHZCAD,XETHXXBT', updatePrice);    
+}, 1000 * 30);
