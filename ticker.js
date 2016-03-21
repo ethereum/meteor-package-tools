8b19078929f7dcf2574086b82c8d51adf4743d1f
Fabian Vogelsteller <fabian@frozeman.de>
2016-03-21 18:50:05 +0100

    changed api endpoint

--- ticker.js
+++ ticker.js
@@ -8,30 +8,30 @@ var updatePrice = function(e, res){
     if(!e && res && res.statusCode === 200) {
         var content = JSON.parse(res.content);
 
-        if(content && content.Response === 'Success' && content.Data){
-            _.each(content.Data, function(item){
-                var name = item.Symbol.toLowerCase();
+        if(content){
+            _.each(content, function(price, key){
+                var name = key.toLowerCase();
 
                 // make sure its a number and nothing else!
-                if(_.isFinite(item.Price)) {
+                if(_.isFinite(price)) {
                     EthTools.ticker.upsert(name, {$set: {
-                        price: String(item.Price),
-                        timestamp: item.LastUpdateTS
+                        price: String(price),
+                        timestamp: null
                     }});
                 }
 
             });
         }
     } else {
-        console.warn('Can not connect to https://www.cryptocompare.com/api to get price ticker data, please check your internet connection.');
+        console.warn('Can not connect to https://mini-api.cryptocompare.com to get price ticker data, please check your internet connection.');
     }
 };
 
 // update right away
-HTTP.get('https://www.cryptocompare.com/api/data/price?fsym=ETH&tsyms=BTC,USD,EUR', updatePrice);
+HTTP.get('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR', updatePrice);
     
 
 // update prices
 Meteor.setInterval(function(){
-    HTTP.get(' https://www.cryptocompare.com/api/data/price?fsym=ETH&tsyms=BTC,USD,EUR', updatePrice);    
+    HTTP.get(' https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR', updatePrice);    
 }, 1000 * 30);
