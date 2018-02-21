e6b62f9d6da55f6372a38a44a7cae2a4d0067206
Konstantin <e@logvik.com>
2018-02-21 14:52:57 +0300

    Update package.js
     api.use('3stack:bignumber@2.0.0', 'client'); - cost ~400Kb, but web3.js already has it

--- package.js
+++ package.js
@@ -16,9 +16,7 @@ Package.onUse(function(api) {
   api.use('tracker', 'client');
 
   //api.use('numeral:numeral@1.5.3', ['client', 'server']);
-  api.use('3stack:bignumber@2.0.0', 'client');
-
-  api.use('ethereum:web3@0.15.1', ['client', 'server']);
+  api.use('ethereum:web3', ['client', 'server']);
   api.use('frozeman:persistent-minimongo@0.1.8', 'client');
   api.use('frozeman:storage@0.1.8', 'client');
 
